65765bfd5122bf50ac6274f20134d5aeeda7e275
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-07-17 11:36:11 +0600

    19-th commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const today1 = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
+}
+console.log(today1["3"])
+
+
 let day = 3
 const today = {
     1: "Monday",
