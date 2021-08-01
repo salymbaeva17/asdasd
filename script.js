1105a76591282248576760f2dacd1c5c70222b0a
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-01 23:58:43 +0600

    75

--- script.js
+++ script.js
@@ -10,6 +10,20 @@
 // console.log(divide(10, 2))
 
 
+// function num2(a) {
+//     if (a > 0) {
+//         return "positive"
+//     } else if (a < 0) {
+//         return "negative"
+//     }
+//     return "equal"
+// }
+//
+// console.log(num2(2))
+// console.log(num2(-2))
+// console.log(num2(0))
+
+
 // function a(number) {
 //     if (number % 2 === 0) {
 //         return "Четное число"
